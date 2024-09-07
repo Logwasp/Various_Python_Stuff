0d1b6a1abb6b948bedc7f133ee5df6c94d09a887
Logan Spencer <logwasp@gmail.com>
2024-09-07 17:43:57 -0500

    Implemented basic cell display in gol.py

--- gol.py
+++ gol.py
@@ -5,21 +5,88 @@ BLUE =      (  0,   0, 255)
 GREEN =     (  0, 255,   0)
 RED =       (255,   0,   0)
 BLACK = (  0,   0,  0)
-(width, height) = (1200, 800)
+(width, height) = (800, 800)
 
 running = True
+
+#Class for each cell
+class Cell:
+    def __init__(self, xpos, ypos, state):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.state = state
+
+    
+
+#Class for the grid
+class Grid:
+    def __init__(self, gridWidth, gridHeight):
+        self.gridWidth = gridWidth
+        self.gridHeight = gridHeight
+
+        self.grid = []
+        
+        for i in range(gridWidth):
+            for j in range(gridHeight):
+                self.grid.append(Cell(i, j, 0))
+
+
+
+#Function to draw the grid
+def drawGrid(Grid):
+    cellsize = width / Grid.gridWidth
+    for i in range(Grid.gridHeight):
+        for j in range(Grid.gridWidth):
+            if Grid.grid[i*Grid.gridWidth + j].state == 1:
+                pygame.draw.rect(screen, WHITE, [j*cellsize, i*cellsize, cellsize, cellsize])
+
+
+
+#Function to count the number of neighbors
+def countNeighbors(grid, cell):
+    count = 0
+    if cell.state == 1:
+        count += 1
+    
+    return count
+
+
+
+#Function to reproduce
+#def reproduce(grid, cells):
+    #for cell in cells:
+       # if cell.state == 1:
+            #if 
+    
+
+        
+        
+
+
+
 
 def main():
     global running, screen
     
     pygame.init()
     screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("TUFF")
+    pygame.display.set_caption("Game of Life")
     screen.fill(BLACK)
 
-    
-    drawPlayer2()
+    realgrid = Grid(40, 40)
+
+
+    realgrid.grid[44].state = 1
+    #print(realgrid.grid[4].state)
+
+    drawGrid(realgrid)
+
+    print(countNeighbors(realgrid, realgrid.grid[44]))
+
     pygame.display.update()
+
+    
+
     
     while running:
         ev = pygame.event.get()
@@ -27,21 +94,17 @@ def main():
         for event in ev:
 
             if event.type == pygame.MOUSEBUTTONUP:
-                drawPlayer1()
                 pygame.display.update()
 
             if event.type == pygame.QUIT:
                 running = False
 
+
+
 def getPos():
     pos = pygame.mouse.get_pos()
     return (pos)
 
-def drawPlayer1():
-    pygame.draw.circle(screen, RED, [200, 400], 25)
-
-def drawPlayer2():
-    pygame.draw.circle(screen, BLUE, [1000, 400], 25)
 
 
 if __name__ == '__main__':
